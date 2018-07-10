65a9ef235c70cc293669ec510abb8be1ebe6b4bb
kk <18810276930@163.com>
2018-07-10 15:07:07 +0800

    增加判断待抓取队列是否满的条件

--- hacker/hacker/kafka_scrapy_tool.py
+++ hacker/hacker/kafka_scrapy_tool.py
@@ -8,6 +8,8 @@ from settings import SCRAPY_KAFKA_HOSTS
 from settings import SCRAPY_KAFKA_TOPIC
 from settings import SCRAPY_KAFKA_SPIDER_CONSUMER_GROUP
 from settings import CLIENT_ID
+from settings import SCRAPY_QUERRY_LENGTH
+from settings import MAX_RECORDS
 from kafka import KafkaConsumer
 
 
@@ -52,6 +54,15 @@ class KafkaSpiderMixin(object):
         print("xxx")
         self.log("kafka_topic URLs from kafka topic '%s'" % topic)
 
+    # 判断scrapy的scheduler队列是否满
+    def is_scrapy_scheduler_query_full(self):
+        # scheduler/enqueued
+        if (self.crawler.stats.get_value('scheduler/enqueued') is None and self.crawler.stats.get_value('scheduler/dequeued') is None):
+            return False
+        elif (self.crawler.stats.get_value('scheduler/enqueued') - self.crawler.stats.get_value('scheduler/dequeued') > SCRAPY_QUERRY_LENGTH):
+            return True
+        else:
+            return False
     def next_request(self):
         """
         按照默认 max_poll_interval_ms 300000ms 处理max_poll_records 500条
@@ -66,10 +77,17 @@ class KafkaSpiderMixin(object):
         #     req = self.make_requests_from_url(url)
         #     if isinstance(req, Request):
         #         self.crawler.engine.crawl(req, spider=self)
-        
-        message = self.consumer.poll(max_records=1)
-        # self.log(u"拉取信息")
-        # print(message)
+
+
+        # if(self.crawler.stats.get_value('scheduler/enqueued')-self.crawler.stats.get_value('scheduler/dequeued')>0):
+        #     print("scheduler queued is full!!!wait ...")
+        #     return
+        print(self.crawler.stats.get_value('scheduler/enqueued'))
+        print(self.crawler.stats.get_value('scheduler/dequeued'))
+        if self.is_scrapy_scheduler_query_full():
+            return
+        message = self.consumer.poll(max_records=MAX_RECORDS)
+
         # TODO 处理单条信息
 
         if message:
@@ -99,20 +117,15 @@ class KafkaSpiderMixin(object):
             pass
 
 
-    # def schedule_next_request(self):
-    #     """Schedules a request if available"""
-    #     req = self.next_request()
-    #     # 这里要对req进行判断，如果是Request就发送到scrapy抓取
-    #     if isinstance(req, Request):
-    #         self.crawler.engine.crawl(req, spider=self)
-
     def spider_idle(self):
         """Schedules a request if available, otherwise waits."""
+        print("----- spdier_idle -----")
         self.next_request()
         raise DontCloseSpider
 
     def item_scraped(self, *args, **kwargs):
         """Avoids waiting for the spider to  idle before scheduling the next request"""
+        print("----- item_scraped -----")
         self.next_request()
 
 
