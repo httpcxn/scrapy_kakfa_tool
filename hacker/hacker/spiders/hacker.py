987f200298d5491140eb6bba2c288f094d250f00
kk <18810276930@163.com>
2018-07-10 15:05:59 +0800

    输出爬虫状态

--- hacker/hacker/spiders/hacker.py
+++ hacker/hacker/spiders/hacker.py
@@ -2,6 +2,7 @@
 
 import scrapy
 from ..kafka_scrapy_tool import ListeningKafkaSpider
+from time import sleep
 class baiduSpider(ListeningKafkaSpider):
     name = "hacker"
     # start_urls = [
@@ -9,9 +10,12 @@ class baiduSpider(ListeningKafkaSpider):
     # ]
 
     def parse(self, response):
-        print("start")
-        self.logger.info(u"开始抓取.")
+        #print("start")
+        #self.logger.info(u"开始抓取.")
         a = response.url
-
-        print(a)
+        # sleep(5)
+        print(self.crawler.stats.get_stats())
+        print(self.crawler.stats.get_value('scheduler/enqueued'))
+        print(self.crawler.stats.get_value('scheduler/dequeued'))
+        # print(a)
         print(u"访问链接成功")
